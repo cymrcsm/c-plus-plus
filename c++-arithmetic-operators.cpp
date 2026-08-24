7158eca98e1ebf87f77b2e6a395ee1fd9b24a85a
Cymer Casem <casemcymer@gmail.com>
2026-08-24 21:09:21 +0800

    Add files via upload

--- c++-arithmetic-operators.cpp
+++ c++-arithmetic-operators.cpp
@@ -18,12 +18,42 @@ namespace subtract_student{
     }
 }
 
+namespace multiply_student{
+    int multiply(int students){
+        return students * 2;
+    }
+    int multiply_4(int students){
+        return students *= 4;
+    }
+}
+
+namespace divide_student{
+    int divide(int students){
+        return students / 4;
+    }
+    int divide_5(int students){
+        return students /= 5;
+    }
+}
+
+namespace remainder_student{
+    int remainder(int students){
+        return students % 2;
+    }
+    int remainder_3(int students){
+        return students %= 3;
+    }
+}
+
 
 int main() {
     using std::cout;
     using std::endl;
     using namespace add_student;
     using namespace subtract_student;
+    using namespace multiply_student;
+    using namespace divide_student;
+    using namespace remainder_student;
 
     int students = 20;
 
@@ -40,7 +70,17 @@ int main() {
     cout << "08/28/26 Attendance:" << endl;
     cout << "total number of students present: " << subtract_3(students) << endl;
     cout << "======================================================" << endl;
-
+    cout << endl;
+    cout << "===============Total Number of Students===============" << endl;
+    cout << "total number of students(Section A)  : " << multiply(students) << endl;
+    cout << "total number of students(Section A-D): " << multiply_4(students) << endl;
+    cout << "======================================================" << endl;
+    cout << endl;
+    cout << "===========Total Number of Students Athletes==========" << endl;
+    cout << "Section A: " << divide(students) << endl;
+    cout << "Section B: " << divide_5(students) << endl;
+    cout << "Section C: " << remainder_3(students) << endl;
+    cout << "Section D: " << remainder(students) << endl;
 
     return 0;
 }
