a4eae385868e3e49e7fff0e0ef6858af5a2fe552
Cymer Casem <casemcymer@gmail.com>
2026-08-25 22:37:59 +0800

    Add files via upload

---
+++ c++-lab1-lab2.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+string full_name;
+string age;
+string date_of_birth;
+string gender;
+string address;
+string contact_number_1;
+string email;
+
+string student_id;
+string course;
+string major;
+string year_level;
+string section;
+string school;
+
+string emergency_contact_name;
+string relationship_to_student;
+string contact_number_2;
+
+int main(){
+    cout << "=================Personal Information=================" << endl;
+    cout << "Enter your full name          : ";
+    getline(cin, full_name);
+    cout << "Enter your age                : ";
+    getline(cin, age);
+    cout << "Enter your date of birth      : ";
+    getline(cin, date_of_birth);
+    cout << "Enter your gender             : ";
+    getline(cin, gender);
+    cout << "Enter your address            : ";
+    getline(cin, address);
+    cout << "Enter your contact number     : +63";
+    getline(cin, contact_number_1);
+    cout << "Enter your email address      : ";
+    getline(cin, email);
+    cout << "=================Academic Information=================" << endl;
+    cout << "Enter your student ID         : ";
+    getline(cin, student_id);
+    cout << "Enter your course             : ";
+    getline(cin, course);
+    cout << "Enter your major              : ";
+    getline(cin, major);
+    cout << "Enter your year level         : ";
+    getline(cin, year_level);
+    cout << "Enter your section            : ";
+    getline(cin, section);
+    cout << "Enter your school             : ";
+    getline(cin, school);
+    cout << "=============Emergency Contact Information============" << endl;
+    cout << "Enter emergency contact name  : ";
+    getline(cin, emergency_contact_name);
+    cout << "Enter relationship to student : ";
+    getline(cin, relationship_to_student);
+    cout << "Enter contact number          : +63";
+    getline(cin, contact_number_2);
+
+
+    cout << "====================Personal Information====================" << endl;
+    cout << "Full Name                : " << full_name << endl;
+    cout << "Age                      : " << age << endl;
+    cout << "Date of Birth            : " << date_of_birth << endl;
+    cout << "Gender                   : " << gender << endl;
+    cout << "Address                  : " << address << endl;
+    cout << "Contact Number           : " << contact_number_1 << endl;
+    cout << "Email Address            : " << email << endl;
+    cout << "====================Academic Information====================" << endl;
+    cout << "Student ID               : " << student_id << endl;
+    cout << "Course                   : " << course << endl;
+    cout << "Major/Specialization     : " << major << endl;
+    cout << "Year level               : " << year_level << endl;
+    cout << "Section                  : " << section << endl;
+    cout << "School/University        : " << school << endl;
+    cout << "================Emergency Contact Information===============" << endl;
+    cout << "Emergency Contact Name   : " << emergency_contact_name << endl;
+    cout << "Relationship to Student  : " << relationship_to_student << endl;
+    cout << "Emergency Contact Number : " << contact_number_2 << endl;
+
+
+    return 0;
+}
